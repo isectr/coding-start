6acb7b671cd10ac74f423f14bb04e2e87b2d542c
isectr <you@example.com>
2016-12-30 00:01:50 +0900

    started blog app

---
+++ week-02-django/my_django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hwaky_zaga\dev\coding-start\week-02-django\my_django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
